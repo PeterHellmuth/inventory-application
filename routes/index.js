9a156912c0963d63f6a8193ac1cf44ff2b1e9247
PeterHellmuth <10523853+PeterHellmuth@users.noreply.github.com>
2023-11-08 14:41:31 -0600

    Add logging for route filepath served

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const path = require("path");
 const router = express.Router();
 const buildPath = path.normalize(path.join(__dirname, "../client/dist"));
 /* GET home page. */
+console.log(path.join(buildPath, "index.html"));
 router.get("/", (req, res, next) => {
   res.sendFile(path.join(buildPath, "index.html"));
 });
